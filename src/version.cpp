f902efc546ec158532a539941e6ca7f7f100df50
Slowyn <y231966mdk@gmail.com>
2018-02-20 13:07:28 +0700

    Change version

--- src/version.cpp
+++ src/version.cpp
@@ -9,7 +9,7 @@
 // Name of client reported in the 'version' message. Report the same name
 // for both bitcoind and bitcoin-qt, to make it harder for attackers to
 // target servers or GUI users specifically.
-const std::string CLIENT_NAME("VVraith");
+const std::string CLIENT_NAME("CrypticCoin");
 
 // Client version number
 #define CLIENT_VERSION_SUFFIX   "-beta"
