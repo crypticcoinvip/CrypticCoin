45be52cadc2f31fe02d3160f809b2f49e3cc52b6
Slowyn <y231966mdk@gmail.com>
2018-02-28 14:36:59 +0700

    Add new test YEAR block

--- src/main.h
+++ src/main.h
@@ -43,7 +43,7 @@ static const int64 PREMINE_AMOUNT = 3039442976 * COIN;
 static const int64 INFLATION = 151972147 * COIN; // Money amount which will be produced in the end of a year
 static const int64 ANNIVERSARY_REWARD = 32904 * COIN; // Bonus for a miner who created anniversary block
 // FIXME: Remove test year blocks
-static const int64 YEAR_BLOCKS[] = { 20, 50, 100, 120, 140, 1048320, 2096640, 3144960, 4193280, 5241600, 6289920 }; // Number of block which will be mined in one year, two, three and etc.
+static const int64 YEAR_BLOCKS[] = { 20, 50, 70, 100, 120, 140, 1048320, 2096640, 3144960, 4193280, 5241600, 6289920 }; // Number of block which will be mined in one year, two, three and etc.
 static const int DISABLE_POS_BLOCK = 1;
 
 static const int64 MIN_TXOUT_AMOUNT = MIN_TX_FEE;
