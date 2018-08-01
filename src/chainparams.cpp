924fe29e2b998765f19abdea95cff69c8981ca07
Egor Lysenko <repo@sfxdx.ru>
2018-08-01 17:14:32 +0700

    Uncomment regrest (with testnet genesis block)

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -272,93 +272,94 @@ static CTestNetParams testNetParams;
 /**
  * Regression test
  */
-//class CRegTestParams : public CChainParams {
-//public:
-//    CRegTestParams() {
-//        strNetworkID = "regtest";
-//        strCurrencyUnits = "REG";
-//        consensus.fCoinbaseMustBeProtected = false;
-//        consensus.nSubsidySlowStartInterval = 0;
-//        consensus.nSubsidyHalvingInterval = 150;
-//        consensus.nMajorityEnforceBlockUpgrade = 750;
-//        consensus.nMajorityRejectBlockOutdated = 950;
-//        consensus.nMajorityWindow = 1000;
-//        consensus.powLimit = uint256S("0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f");
-//        consensus.nPowAveragingWindow = 17;
-//        assert(maxUint/UintToArith256(consensus.powLimit) >= consensus.nPowAveragingWindow);
-//        consensus.nPowMaxAdjustDown = 0; // Turn off adjustment down
-//        consensus.nPowMaxAdjustUp = 0; // Turn off adjustment up
-//        consensus.nPowTargetSpacing = 2.5 * 60;
-//        consensus.vUpgrades[Consensus::BASE_SPROUT].nProtocolVersion = 170002;
-//        consensus.vUpgrades[Consensus::BASE_SPROUT].nActivationHeight =
-//            Consensus::NetworkUpgrade::ALWAYS_ACTIVE;
-//        consensus.vUpgrades[Consensus::UPGRADE_TESTDUMMY].nProtocolVersion = 170002;
-//        consensus.vUpgrades[Consensus::UPGRADE_TESTDUMMY].nActivationHeight =
-//            Consensus::NetworkUpgrade::NO_ACTIVATION_HEIGHT;
-//        consensus.vUpgrades[Consensus::UPGRADE_OVERWINTER].nProtocolVersion = 170003;
-//        consensus.vUpgrades[Consensus::UPGRADE_OVERWINTER].nActivationHeight =
-//            Consensus::NetworkUpgrade::NO_ACTIVATION_HEIGHT;
-//
-//        pchMessageStart[0] = 0xaa;
-//        pchMessageStart[1] = 0xe8;
-//        pchMessageStart[2] = 0x3f;
-//        pchMessageStart[3] = 0x5f;
-//        nDefaultPort = 18344;
-//        nMaxTipAge = 24 * 60 * 60;
-//        nPruneAfterHeight = 1000;
-//        const size_t N = 48, K = 5;
-//        BOOST_STATIC_ASSERT(equihash_parameters_acceptable(N, K));
-//        nEquihashN = N;
-//        nEquihashK = K;
-//
-//        genesis = CreateGenesisBlock(
-//            1296688602,
-//            uint256S("0x0000000000000000000000000000000000000000000000000000000000000009"),
-//            ParseHex("01936b7db1eb4ac39f151b8704642d0a8bda13ec547d54cd5e43ba142fc6d8877cab07b3"),
-//            0x200f0f0f, 4, 0);
-//        consensus.hashGenesisBlock = genesis.GetHash();
-//        assert(consensus.hashGenesisBlock == uint256S("0x029f11d80ef9765602235e1bc9727e3eb6ba20839319f761fee920d63401e327"));
-//        assert(genesis.hashMerkleRoot == uint256S("0x1927ef984ff76fead7fb8b0304d0973326fb33289de1fa97e9b3d823e14fd8cb"));
-//
-//        vFixedSeeds.clear(); //! Regtest mode doesn't have any fixed seeds.
-//        vSeeds.clear();  //! Regtest mode doesn't have any DNS seeds.
-//
-//        fMiningRequiresPeers = false;
-//        fDefaultConsistencyChecks = true;
-//        fRequireStandard = false;
-//        fMineBlocksOnDemand = true;
-//        fTestnetToBeDeprecatedFieldRPC = false;
-//
-//        checkpointData = (CCheckpointData){
-//            boost::assign::map_list_of
-//            ( 0, uint256S("0f9188f13cb7b2c71f2a335e3a4fc328bf5beb436012afca590b1a11466e2206")),
-//            0,
-//            0,
-//            0
-//        };
-//        // These prefixes are the same as the testnet prefixes
-//        base58Prefixes[PUBKEY_ADDRESS]     = {0x1D,0x25};
-//        base58Prefixes[SCRIPT_ADDRESS]     = {0x1C,0xBA};
-//        base58Prefixes[SECRET_KEY]         = {0xEF};
-//        // do not rely on these BIP32 prefixes; they are not specified and may change
-//        base58Prefixes[EXT_PUBLIC_KEY]     = {0x04,0x35,0x87,0xCF};
-//        base58Prefixes[EXT_SECRET_KEY]     = {0x04,0x35,0x83,0x94};
-//        base58Prefixes[ZCPAYMENT_ADDRRESS] = {0x16,0xB6};
-//        base58Prefixes[ZCVIEWING_KEY]      = {0xA8,0xAC,0x0C};
-//        base58Prefixes[ZCSPENDING_KEY]     = {0xAC,0x08};
-//
-//        // Founders reward script expects a vector of 2-of-3 multisig addresses
-//        vFoundersRewardAddress = { /* "t2FwcEhFdNXuFMv1tcYwaBJtYVtMj8b1uTg" */ };
-//        assert(vFoundersRewardAddress.size() <= consensus.GetLastFoundersRewardBlockHeight());
-//    }
-//
-//    void UpdateNetworkUpgradeParameters(Consensus::UpgradeIndex idx, int nActivationHeight)
-//    {
-//        assert(idx > Consensus::BASE_SPROUT && idx < Consensus::MAX_NETWORK_UPGRADES);
-//        consensus.vUpgrades[idx].nActivationHeight = nActivationHeight;
-//    }
-//};
-//static CRegTestParams regTestParams;
+class CRegTestParams : public CChainParams {
+public:
+    CRegTestParams() {
+        strNetworkID = "regtest";
+        strCurrencyUnits = "REG";
+        consensus.fCoinbaseMustBeProtected = false;
+        consensus.nSubsidySlowStartInterval = 0;
+        consensus.nSubsidyHalvingInterval = 150;
+        consensus.nMajorityEnforceBlockUpgrade = 750;
+        consensus.nMajorityRejectBlockOutdated = 950;
+        consensus.nMajorityWindow = 1000;
+        consensus.powLimit = uint256S("0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f");
+        consensus.nPowAveragingWindow = 17;
+        assert(maxUint/UintToArith256(consensus.powLimit) >= consensus.nPowAveragingWindow);
+        consensus.nPowMaxAdjustDown = 0; // Turn off adjustment down
+        consensus.nPowMaxAdjustUp = 0; // Turn off adjustment up
+        consensus.nPowTargetSpacing = 2.5 * 60;
+        consensus.vUpgrades[Consensus::BASE_SPROUT].nProtocolVersion = 170002;
+        consensus.vUpgrades[Consensus::BASE_SPROUT].nActivationHeight =
+            Consensus::NetworkUpgrade::ALWAYS_ACTIVE;
+        consensus.vUpgrades[Consensus::UPGRADE_TESTDUMMY].nProtocolVersion = 170002;
+        consensus.vUpgrades[Consensus::UPGRADE_TESTDUMMY].nActivationHeight =
+            Consensus::NetworkUpgrade::NO_ACTIVATION_HEIGHT;
+        consensus.vUpgrades[Consensus::UPGRADE_OVERWINTER].nProtocolVersion = 170003;
+        consensus.vUpgrades[Consensus::UPGRADE_OVERWINTER].nActivationHeight =
+            Consensus::NetworkUpgrade::NO_ACTIVATION_HEIGHT;
+
+        pchMessageStart[0] = 0xaa;
+        pchMessageStart[1] = 0xe8;
+        pchMessageStart[2] = 0x3f;
+        pchMessageStart[3] = 0x5f;
+        nDefaultPort = 18344;
+        nMaxTipAge = 24 * 60 * 60;
+        nPruneAfterHeight = 1000;
+        const size_t N = 48, K = 5;
+        BOOST_STATIC_ASSERT(equihash_parameters_acceptable(N, K));
+        nEquihashN = N;
+        nEquihashK = K;
+
+        genesis = CreateGenesisBlock(
+            1533007800,
+            uint256S("0x000000000000000000000000000000000000000000000000000000000000264e"),
+            ParseHex("00324610994396bd15f001091c2c68bde62bd353d207c584a79ecf80e18d5300aa2ea973909012cf50610a216f4c8ac78921802601e1073c959dacf451ff7e1bdc9e30fd5b0f52ffdad4090661e7b62eb0bfa4ac0332656c6c36bbc3eb6c516574f39395ba655ab95f080a308d91072041931bd08f69cbb149d22bf8d9270580db38b8c1c0db70f6d295b9971f69fed0909275300f1b4d6ff118a9b2e1da11967eb26340115d256a04ae2f47591a19db843a331b615306a29b7c3fd05404b966964ba777cb7cf558d093d066f6d4a2d81620070cbbd2498fea39aef642b6d2fe520154a95101f9078b91937488e4724b05b2f54e6f56855c5ad8e82c0b3648de754da592aecba1bf12339c7cfcac7c3be31d343b0e056699076c6443e5597b10125b2afd3bf90d3495e996dfe975e7d44568ed378ccde4de57a64457364e7891a57781be6ebb63b85c1b8f55c63cf68f0104f09d8a8e4d46b667478530d0944e107c3529ac47f1c769e4a78e4da64df80f8cef297f67f13bced316a5127bf39f4f8bcd0a03cb5456cd31ac577a932c3bb91222acd13c44ce3e041b91d54b212d3575c49e0348c60fcb14d5d9e3c3a2a54dfc6654c820de8686185b3ae004d868f4e901034a14a785994a362fff0d06e6b451a9de9f0b59b223d2eae2000961308bca171399c668c508796fcb1a5237bd27605de6ebd17f0a1536641443d370edc74245bbc76e511f18365b057c2043bcf0f3f0a4d9a84bc6deb7d61d021a3b7d7890194995a253d2106d6d41453cf24cddbb4621fb02db2c7cc43d96ef96b9cf91b7d23246fb6b3af8dc171f24369cd5d3a8c61b7faa82b15f3577ad8e6016c3b82b38e22783e88cb9904886a3dab5b709e9379f4b41411d14f0a4dfaf47b72b18bdca7f4f1e39ceddb75f61c9456809acdc35b1f6b6dcb853f64a71531841b2011e526be6870cdc763b02dcde6948b6eb2fb926ec7c25f53b7d21451dfa8ee88b22e6367286165bee4516e1d75697d724edd29f52a9ed69ead0dba93d05481bd057864a36143ffa9da1efd83ad96c969738c5e80d7a70a38193538aaa1846161439e07f3d315fcc3f139edbf4bc9be060e283e3ff78a4085555b65c3fdb4445eed0203498a7a65eb5ecd0ece44dd81773a05149640c4da857492baea334f7caccd75ebe2d7e6d9e04f039acac14312dfe81947654c41ac534fdb165f4134f28bd7bc57f8b09d808ba9cf7a63f63a4de777f184b1a003ba8e8bf6241949d2edbbc4284161f5ff38c29053d71ec37eb6b851c8d8bfcf99f6a4c5fa5b00c5484508a129875be4ca57ed67026eb1ca23b337f107a7d79851d9cf55cd10782f0e6b82efab7bc7b4a130ac7901f7a3887e02684d9d464823635bd371a04159eba627ebc1bbdead0c43df5d3704df54cdb7980022f524a7e6444f979b4539e9da50abd05d437073d1cea02bb548f93c7fa80e4a5d4c5005da60c1a369307560cd973da8e24fe9ba0d2f949829a462d7a4be03f5fda5da82888c778fc840890532d52e0b93a16a90544de86a24f0e195546f20cc221f8834687db6f211bf1ad4f03220b5bde9a1586f77aaa035031fc0d3409281ba709c9c4314bbf20aba16c7b099f117ddf0d71ecd6298d71bce7ddbc99f7e9f935b32b693addb30538f2f88bd1c01f0b9780d5358bf9068a1ada2dde069589dc5f2977b3d853e8925252e703267ddb86bc10764e5fdd44eace8e1de5fdade309aa6618d44b263f498ece1b6b501b902985cac5be445a61a5be59d1059e4b932aa03c75024cc2a660f96a9db958be3c0a8555451f5ff2c507be26c9ff73fada1f3965fa849707b400f2fc49ab2c8c048665d3f5ce45ea28f304ff25654981218bb79cb241a260de2844bca5263043e3"),
+            0x1f07ffff, 4, 0);
+        consensus.hashGenesisBlock = genesis.GetHash();
+        assert(consensus.hashGenesisBlock == uint256S("0x000095aa3b6953c0757dbd0c6ba828fefab484a15eec5ea6c3d2776e6ea4b38c"));
+        assert(genesis.hashMerkleRoot == uint256S("0x1927ef984ff76fead7fb8b0304d0973326fb33289de1fa97e9b3d823e14fd8cb"));
+
+        vFixedSeeds.clear(); //! Regtest mode doesn't have any fixed seeds.
+        vSeeds.clear();  //! Regtest mode doesn't have any DNS seeds.
+
+        fMiningRequiresPeers = false;
+        fDefaultConsistencyChecks = true;
+        fRequireStandard = false;
+        fMineBlocksOnDemand = true;
+        fTestnetToBeDeprecatedFieldRPC = false;
+
+        checkpointData = (CCheckpointData){
+            boost::assign::map_list_of
+            ( 0, uint256S("0f9188f13cb7b2c71f2a335e3a4fc328bf5beb436012afca590b1a11466e2206")),
+            0,
+            0,
+            0
+        };
+        // These prefixes are the same as the testnet prefixes
+        base58Prefixes[PUBKEY_ADDRESS]     = {0x1D,0x25};
+        base58Prefixes[SCRIPT_ADDRESS]     = {0x1C,0xBA};
+        base58Prefixes[SECRET_KEY]         = {0xEF};
+        // do not rely on these BIP32 prefixes; they are not specified and may change
+        base58Prefixes[EXT_PUBLIC_KEY]     = {0x04,0x35,0x87,0xCF};
+        base58Prefixes[EXT_SECRET_KEY]     = {0x04,0x35,0x83,0x94};
+        base58Prefixes[ZCPAYMENT_ADDRRESS] = {0x16,0xB6};
+        base58Prefixes[ZCVIEWING_KEY]      = {0xA8,0xAC,0x0C};
+        base58Prefixes[ZCSPENDING_KEY]     = {0xAC,0x08};
+
+        // Founders reward script expects a vector of 2-of-3 multisig addresses
+        vFoundersRewardAddress = {};
+        assert(vFoundersRewardAddress.size() <= consensus.GetLastFoundersRewardBlockHeight());
+    }
+
+    void UpdateNetworkUpgradeParameters(Consensus::UpgradeIndex idx, int nActivationHeight)
+    {
+        assert(idx > Consensus::BASE_SPROUT && idx < Consensus::MAX_NETWORK_UPGRADES);
+        consensus.vUpgrades[idx].nActivationHeight = nActivationHeight;
+    }
+};
+static CRegTestParams regTestParams;
+
 
 static CChainParams *pCurrentParams = 0;
 
@@ -374,7 +375,7 @@ CChainParams &Params(CBaseChainParams::Network network) {
         case CBaseChainParams::TESTNET:
             return testNetParams;
         case CBaseChainParams::REGTEST:
-//            return regTestParams;
+            return regTestParams;
         default:
             assert(false && "Unimplemented network");
             return mainParams;
@@ -387,7 +388,7 @@ void SelectParams(CBaseChainParams::Network network) {
 
     // Some python qa rpc tests need to enforce the coinbase consensus rule
     if (network == CBaseChainParams::REGTEST && mapArgs.count("-regtestprotectcoinbase")) {
-//        regTestParams.SetRegTestCoinbaseMustBeProtected();
+        regTestParams.SetRegTestCoinbaseMustBeProtected();
     }
 }
 
@@ -433,5 +434,5 @@ std::string CChainParams::GetFoundersRewardAddressAtIndex(int i) const {
 
 void UpdateNetworkUpgradeParameters(Consensus::UpgradeIndex idx, int nActivationHeight)
 {
-//    regTestParams.UpdateNetworkUpgradeParameters(idx, nActivationHeight);
+    regTestParams.UpdateNetworkUpgradeParameters(idx, nActivationHeight);
 }
