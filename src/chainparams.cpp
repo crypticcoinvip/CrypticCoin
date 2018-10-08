ba5f883dae510674764410adc163ba4bcb9ae06f
SevenSwen <tihonenko.v.v@gmail.com>
2018-10-08 18:04:02 +0700

    hardfork testnet

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -86,7 +86,7 @@ public:
         strCurrencyUnits = "CRYP";
         consensus.fCoinbaseMustBeProtected = true;
         consensus.nSubsidySlowStartInterval = 20000;
-        consensus.nSubsidyHalvingInterval = 1048320;
+        consensus.nSubsidyHalvingInterval = 10483200;
         consensus.nMajorityEnforceBlockUpgrade = 750;
         consensus.nMajorityRejectBlockOutdated = 950;
         consensus.nMajorityWindow = 4000;
@@ -152,7 +152,7 @@ public:
 
         vFixedSeeds = std::vector<SeedSpec6>(pnSeed6_main, pnSeed6_main + ARRAYLEN(pnSeed6_main));
 
-        fMiningRequiresPeers = false;
+        fMiningRequiresPeers = true;
         fDefaultConsistencyChecks = false;
         fRequireStandard = true;
         fMineBlocksOnDemand = false;
@@ -188,7 +188,7 @@ public:
         strCurrencyUnits = "TAС";
         consensus.fCoinbaseMustBeProtected = true;
         consensus.nSubsidySlowStartInterval = 20000;
-        consensus.nSubsidyHalvingInterval = 1048320;
+        consensus.nSubsidyHalvingInterval = 10483200;
         consensus.nMajorityEnforceBlockUpgrade = 51;
         consensus.nMajorityRejectBlockOutdated = 75;
         consensus.nMajorityWindow = 400;
