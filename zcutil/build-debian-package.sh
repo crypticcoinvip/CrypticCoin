f8e7c0a228f30e29e3f84b2fdf0fb1587a301a85
SevenSwen <tihonenko.v.v@gmail.com>
2018-10-23 15:42:39 +0700

    return lintian

--- zcutil/build-debian-package.sh
+++ zcutil/build-debian-package.sh
@@ -70,5 +70,5 @@ dpkg-gencontrol -P$BUILD_DIR -v$DEBVERSION
 fakeroot dpkg-deb --build $BUILD_DIR
 cp $BUILD_PATH/$PACKAGE_NAME-$PACKAGE_VERSION-amd64.deb $SRC_PATH
 # Analyze with Lintian, reporting bugs and policy violations
-# lintian -i $SRC_PATH/$PACKAGE_NAME-$PACKAGE_VERSION-amd64.deb
+lintian -i $SRC_PATH/$PACKAGE_NAME-$PACKAGE_VERSION-amd64.deb
 exit 0
