fc07388031e537689521cc69d0750ddc14852dd9
Stanislav Shihalev <sshihalev@sfxdx.ru>
2019-02-21 15:55:44 +0700

    add missing file

---
+++ src/masternodes/dpos_types.h
@@ -0,0 +1,17 @@
+#ifndef MASTERNODES_DPOS_TYPES_H
+#define MASTERNODES_DPOS_TYPES_H
+
+#include "../uint256.h"
+#include "../arith_uint256.h"
+
+namespace dpos
+{
+using Round = uint16_t;
+using TxId = uint256;
+using TxIdSorted = arith_uint256;
+using BlockHash = uint256;
+
+}
+
+#endif //MASTERNODES_DPOS_TYPES_H
+
