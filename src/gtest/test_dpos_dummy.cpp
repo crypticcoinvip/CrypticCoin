8042782d3453dc5495bf2af4c403736ed89b9d00
Egor Lysenko <elysenko@sfxdx.ru>
2019-02-21 16:35:39 +0700

    dpos test: add test with empty block

--- src/gtest/test_dpos_dummy.cpp
+++ src/gtest/test_dpos_dummy.cpp
@@ -1,31 +1,102 @@
 #include <gtest/gtest.h>
 
-#include "../chain.h"
-#include "../chainparams.h"
-#include "../random.h"
-#include "../main.h"
-#include "../consensus/validation.h"
-#include "../masternodes/dpos_controller.h"
-
-namespace
-{
-    CBlockIndex * getChainTip()
-    {
-        LOCK(cs_main);
-        return chainActive.Tip();
-    }
-}
+#include "../masternodes/dpos_voter.h"
 
 TEST(dPoS, NoTransactionsMeansEmptyBlock)
 {
-    CValidationState state{};
-    SelectParams(CBaseChainParams::REGTEST);
-    UpdateNetworkUpgradeParameters(Consensus::UPGRADE_OVERWINTER, Consensus::NetworkUpgrade::ALWAYS_ACTIVE);
-    UpdateNetworkUpgradeParameters(Consensus::UPGRADE_SAPLING, Consensus::NetworkUpgrade::ALWAYS_ACTIVE);
-//    auto consensusParams = Params().GetConsensus();
+    std::vector<CMasternode::ID> masternodeIds;
+    for (uint64_t i = 0; i < 32; i++) {
+        masternodeIds.push_back(ArithToUint256(arith_uint256{i}));
+    }
 
-    EXPECT_EQ(getChainTip(), nullptr);
-    ASSERT_DEATH(dpos::getController()->isEnabled(), "nHeight >= 0");
-    EXPECT_TRUE(ActivateBestChain(state));
-    EXPECT_EQ(getChainTip(), nullptr);
+    dpos::CDposVoter::Callbacks callbacks;
+
+    callbacks.validateTxs = [&](const std::map<TxIdSorted, CTransaction>&) {
+        return true;
+    };
+    callbacks.validateBlock = [&](const CBlock& b, const std::map<TxIdSorted, CTransaction>& txs, bool checkTxs) {
+        return true;
+    };
+
+    std::array<dpos::CDposVoter, 32> voters;
+    for (uint64_t i = 0; i < 32; i++) {
+        voters[i].minQuorum = 23;
+        voters[i].numOfVoters = 32;
+        voters[i].setVoting(uint256S("0xB101"), callbacks, true, masternodeIds[i]);
+    }
+
+    CBlock viceBlock;
+    viceBlock.hashPrevBlock = uint256S("0xB101");
+    viceBlock.nRoundNumber = 1;
+    dpos::CDposVoter::Output res;
+    for (uint64_t i = 0; i < 23; i++) {
+        { // wrong round check
+            viceBlock.nRoundNumber = static_cast<dpos::Round>(i * 2);
+            const size_t initSize = voters[i].v[viceBlock.hashPrevBlock].viceBlocks.size();
+            const auto empty = voters[i].applyViceBlock(viceBlock);
+            ASSERT_TRUE(empty.vTxVotes.empty());
+            ASSERT_TRUE(!empty.blockToSubmit);
+            ASSERT_TRUE(empty.vRoundVotes.empty());
+            ASSERT_TRUE(empty.vErrors.empty());
+            ASSERT_EQ(voters[i].v[viceBlock.hashPrevBlock].viceBlocks[viceBlock.GetHash()].GetHash(), viceBlock.GetHash());
+            viceBlock.nRoundNumber = 1;
+        }
+
+        res += voters[i].applyViceBlock(viceBlock);
+
+        ASSERT_TRUE(res.vTxVotes.empty());
+        ASSERT_TRUE(!res.blockToSubmit);
+        ASSERT_TRUE(res.vErrors.empty());
+        ASSERT_EQ(voters[i].v[viceBlock.hashPrevBlock].viceBlocks[viceBlock.GetHash()].GetHash(), viceBlock.GetHash());
+
+        dpos::CTxVote voteWant;
+        voteWant.voter = masternodeIds[i];
+        voteWant.nRound = 1;
+        voteWant.tip = viceBlock.hashPrevBlock;
+        voteWant.choice = dpos::CVoteChoice{viceBlock.GetHash(), dpos::CVoteChoice::Decision::YES};
+
+        ASSERT_EQ(res.vRoundVotes.size(), i + 1);
+        ASSERT_EQ(res.vRoundVotes[i], voteWant);
+
+        const auto voter0out = voters[0].applyRoundVote(res.vRoundVotes[i]);
+        if (i == 23 - 1) {
+            // final vote
+            ASSERT_TRUE(voter0out.vTxVotes.empty());
+            ASSERT_TRUE(voter0out.vRoundVotes.empty());
+            ASSERT_TRUE(voter0out.vErrors.empty());
+            ASSERT_TRUE(voter0out.blockToSubmit);
+            ASSERT_EQ(voter0out.blockToSubmit->block.GetHash(), viceBlock.GetHash());
+            ASSERT_EQ(voter0out.blockToSubmit->vApprovedBy.size(), 23);
+        } else {
+            // not final
+            ASSERT_TRUE(voter0out.vTxVotes.empty());
+            ASSERT_TRUE(voter0out.vRoundVotes.empty());
+            ASSERT_TRUE(voter0out.vErrors.empty());
+            ASSERT_FALSE(voter0out.blockToSubmit);
+        }
+
+        { // duplicate check
+            const auto empty = voters[i].applyViceBlock(viceBlock);
+            ASSERT_TRUE(empty.vTxVotes.empty());
+            ASSERT_TRUE(!empty.blockToSubmit);
+            ASSERT_TRUE(empty.vRoundVotes.empty());
+            ASSERT_TRUE(empty.vErrors.empty());
+        }
+
+        {
+            const auto empty = voters[i].doRoundVoting();
+            ASSERT_TRUE(empty.vTxVotes.empty());
+            ASSERT_TRUE(!empty.blockToSubmit);
+            ASSERT_TRUE(empty.vRoundVotes.empty());
+            ASSERT_TRUE(empty.vErrors.empty());
+        }
+
+        {
+            const auto empty = voters[i].doTxsVoting();
+            ASSERT_TRUE(empty.vTxVotes.empty());
+            ASSERT_TRUE(!empty.blockToSubmit);
+            ASSERT_TRUE(empty.vRoundVotes.empty());
+            ASSERT_TRUE(empty.vErrors.empty());
+        }
+    }
 }
